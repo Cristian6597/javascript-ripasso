0f65651f3b0b001b1dc4ada4fa50c620379e92d9
cristian ercolani <cristianercolani6597@gmail.com>
2024-10-28 17:27:39 +0100

    reso codice più snello e versatile

--- todos/index.js
+++ todos/index.js
@@ -1,25 +1,50 @@
+const baseUrl = 'https://jsonplaceholder.typicode.com'
 const loading = document.querySelector('#todos .loading');
+const todoDiv = document.querySelector('#todos'); 
 
+const createTodoDiv = (todo) => {
+    const div = document.createElement('div');
+    const input = document.createElement('input');
+    input.type = "checkbox";
+    input.id = todo.id;
+    input.checked = todo.completed;
+    input.onchange = handleCheckbox;
+    const label = document.createElement('label');
+    label.textContent = todo.title;
+    div.appendChild(input);
+    div.appendChild(label);
+    return div;
+}
+//manager delle funzionalità
 
-fetch('https://jsonplaceholder.typicode.com/todos')
+const todoManager = () => { //closeure
+    let state = []; //lista degli elementi che di base è vuota
+    return {
+        set: function(newState) {
+            state = newState;
+            this.render(); //ogni volta che c'è un nuovo stato, renderizza in pagina
+        },
+        render: function() { //prendere i todo e le visualizza nel html
+            todoDiv.innerHTML = ""; //cancella tutta la lista così ogni volta che reinderizza la fetch abbiamo il nuovo stato in cima
+            state.forEach(element => {
+                todoDiv.appendChild(createTodoDiv(element));
+            })
+        },
+        add: function (newElement) {
+            state.unshift(newElement);
+            this.render();
+        }
+    }
+}
+
+const firstTodos = todoManager();
+
+fetch(baseUrl + '/todos')
 .then(response => response.json())
 .then(todos => {
-    const todoDiv = document.querySelector('#todos'); 
     const loading = document.querySelector('#todos .loading'); //document.querySelector sostituisce getElementbyID/class
         loading.remove();
-        todos.forEach(todo => {
-            const div = document.createElement('div');
-            const input = document.createElement('input');
-            input.type = "checkbox";
-            input.id = todo.id;
-            input.checked = todo.completed;
-            input.onchange = handleCheckbox;
-            const label = document.createElement('label');
-            label.textContent = todo.title;
-            div.appendChild(input);
-            div.appendChild(label);
-            todoDiv.appendChild(div);
-        });
+        firstTodos.set(todos);
     })
     .catch((err) => {
         console.log('err: ', err);
@@ -29,8 +54,12 @@ fetch('https://jsonplaceholder.typicode.com/todos')
 const handleCheckbox = (event) => {
     console.log(event.target.checked);
     fetch(
-        'https://jsonplaceholder.typicode.com/todos/' + event.target.id,
-        { method: 'PUT' }
+        baseUrl + '/todos/' + event.target.id,
+        { 
+            method: 'PATCH', 
+            body: JSON.stringify ({completed: event.target.checked}) ,
+            headers: { "Content-Type": "application/json"}
+        }
     )
     .then(response => response.json())
     .then(todo => {
@@ -42,3 +71,30 @@ const handleCheckbox = (event) => {
     })
 
 }
+//aggiunge un nuovo elemento in fondo al form
+
+const todoForm = document.querySelector('#todoForm');
+// todoForm.onsubmit = () => {}
+    todoForm.addEventListener('submit', (event) => {
+        event.preventDefault(); //se ci sono delle azioni di default queste vengono evitate
+        const title = event.target.children.title //title viene dal html
+        fetch(baseUrl + '/todos', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: title.value,
+                completed: false,
+                userId: 1
+            }),
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(response => response.json())
+        .then(newTodo => {
+            firstTodos.add(newTodo);
+
+        })
+    })
+
+// crud = create read uptade e delete
+// metodi : Get, Post, Put, Patch, Delete
+
+//per prendere le API, usare postman e insomnia (siti)
